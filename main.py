65b56e7dff3051ca22e504667c38a3b1ede09d40
andrewens <andrewens4@gmail.com>
2024-04-30 19:24:17 -0500

    Python server serves Ryan's Firehouse Subs (TM) HTML from index.html

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 HOST = "10.235.209.94"
 PORT = 1932
 
-HTML_FILE = "<html><body>Hello World!</body></html>"
+INDEX_HTML_PATH = "index.html"
+with open(INDEX_HTML_PATH, "r") as f:
+    html_file = f.read()
 
 class FirehouseHTTP(BaseHTTPRequestHandler):
 
@@ -14,7 +16,7 @@ class FirehouseHTTP(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
-        self.wfile.write(bytes(HTML_FILE, "utf-8"))
+        self.wfile.write(bytes(html_file, "utf-8"))
 
 
 server = HTTPServer((HOST, PORT), FirehouseHTTP)
